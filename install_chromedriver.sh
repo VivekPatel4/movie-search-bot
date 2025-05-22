b22534e9506c87abeed29db419935f42bfd237b5
VivekPatel4 <vp099909@gmail.com>
2025-05-22 10:49:00 +0530

    Delete install_chromedriver.sh

--- install_chromedriver.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-apt-get update
-apt-get install -y chromium chromium-driver
-ln -s /usr/lib/chromium/chromium /usr/bin/chrome
-ln -s /usr/lib/chromium/chromedriver /usr/bin/chromedriver
